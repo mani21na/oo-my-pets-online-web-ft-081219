47668bf45627ef8aca1f84f70495c47cc0352212
Hanna Lee <mani21na@gmail.com>
2019-08-28 04:35:09 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
   # code goes here
+  attr_reader :name, :owner, :mood
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def owner=(owner)
+    @owner = owner
+  end
+  
+  def mood=(mood)
+    @mood = mood
+  end
+  
+  #calss methods
+  def self.all
+    @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,70 @@
+require 'pry'
+
 class Owner
   # code goes here
+  attr_reader :name, :species
+ 
+  @@all =[]
+  
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @@all << self
+  end
+ 
+  def say_species
+    "I am a #{@species}."
+  end 
+  
+  #owner instance methods
+  def cats 
+    Cat.all.select{|cat| cat if cat.owner == self}
+  end
+  
+  def dogs 
+    Dog.all.select{|dog| dog if dog.owner == self}
+  end
+  
+  def buy_cat(name)
+    a_cat = Cat.new(name, self)
+  end
+  
+  def buy_dog(name)
+    a_dog = Dog.new(name, self)
+  end
+  
+  def walk_dogs
+    Dog.all.map{|dog| dog.mood = "happy" if dog.owner == self}
+  end  
+  
+  def feed_cats
+    Cat.all.map{|cat| cat.mood = "happy" if cat.owner == self}
+  end
+  
+  def sell_pets
+    pet_list = []
+    pet_list << cats
+    pet_list << dogs
+    pet_list.map do |pet|
+      pet.mood = "nervous"
+    end
+    #binding.pry
+  end 
+  
+  def list_pets
+    return "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+  end
+  
+  #class methods
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    self.all.size
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
 end
